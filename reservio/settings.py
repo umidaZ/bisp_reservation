592425aa34162a9ac1401ccddad741596a3cbadb
umidaZ <100193609+umidaZ@users.noreply.github.com>
2024-03-26 08:45:18 +0500

    Update settings.py

--- reservio/settings.py
+++ reservio/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['bisp-restaurant-reservation.onrender.com']
+ALLOWED_HOSTS = ['bisp-restaurants-reservation.onrender.com']
 
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
