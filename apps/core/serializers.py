28ed3813810c98d2949e6c8205e772eb018eedd4
umidaZ <umidaismoilova0217@gmail.com>
2024-03-27 02:42:54 +0500

    changes

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from apps.core.models import User
+from apps.restaurant.models import Customer
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
--- apps/restaurant/apps.py
+++ apps/restaurant/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RestaurantConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.restaurant'
+
+    def ready(self):
+        import apps.restaurant.signals
